8d9d5dd27375eb29f82e9684871d869c72d8f37f
Adam Doamński <s13075@pjwstk.edu.pl>
2022-11-27 19:01:21 +0100

    TDD Test servisu zaliczony po zaimplementowaniu metody

--- src/main/java/com/pjatkInz/logReviewer/service/ApplicationService.java
+++ src/main/java/com/pjatkInz/logReviewer/service/ApplicationService.java
@@ -1,15 +1,31 @@
 package com.pjatkInz.logReviewer.service;
 
 import com.pjatkInz.logReviewer.dto.ApplicationDto;
+import com.pjatkInz.logReviewer.dto.mapper.ApplicationMapperImpl;
+import com.pjatkInz.logReviewer.model.Application;
+import com.pjatkInz.logReviewer.repository.ApplicationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ApplicationService {
 
+    private final ApplicationRepository applicationRepository;
+    private final ApplicationMapperImpl applicationMapper;
+
+    public ApplicationService(ApplicationRepository applicationRepository, ApplicationMapperImpl applicationMapper){
+        this.applicationRepository = applicationRepository;
+        this.applicationMapper = applicationMapper;
+    }
+
     public List<ApplicationDto> getApplications() {
-        return new ArrayList<ApplicationDto>();
+        Iterable<Application> all = applicationRepository.findAll();
+        return StreamSupport.stream(all.spliterator(),false)
+                .map(application -> applicationMapper.applicationToApplicationDto(application))
+                .collect(Collectors.toList());
     }
 }
